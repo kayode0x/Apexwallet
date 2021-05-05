91847c6d1865ad9ceb29dd13d253cdac8ad25879
Kayode Ogunamkinwa <khizzyjr@gmail.com>
2021-05-05 07:03:16 -0400

    built landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+// import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 
 
 function App() {
-  const matches = useMediaQuery('(max-width:1023px)');
+  // const matches = useMediaQuery('(max-width:1023px)');
   
   return (
 		<Router>
