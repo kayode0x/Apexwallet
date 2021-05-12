91e25a6c61db833be532af514687294cef9e8e49
Kayode Ogunamkinwa <khizzyjr@gmail.com>
2021-05-11 21:50:02 -0400

    completed template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AuthContextProvider } from './components/Auth/AuthContext';
 import Routes from './Routes';
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 
 function App() {
